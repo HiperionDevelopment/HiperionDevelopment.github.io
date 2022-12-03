83b4929cb7c3c5e0852208a5b18b5eb5a661fd35
Seth Kitchen <seth.kitchen@aenetworks.com>
2022-12-03 12:06:01 -0600

    hiperion beta

--- ostrich/ostrich/lib/Pages/BasePage.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-// ignore: must_be_immutable
-abstract class BasePage extends StatefulWidget {
-  final String name;
-  final String path;
-
-  BasePage({required Key key, required this.name, required this.path}) : super(key: key);
-}
